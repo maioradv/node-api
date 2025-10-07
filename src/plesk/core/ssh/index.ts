cfa4cade93bf9c16a8db8d859846483707af9a3f
Roberto Colabene <robertocolabene@maior.team>
2025-10-07 09:59:49 +0200

    dev

--- src/plesk/core/ssh/index.ts
+++ src/plesk/core/ssh/index.ts
@@ -17,30 +17,30 @@ export class PleskSSh {
     return this.client.executeCommand(command)
   }
 
-  yarnBuild(user:string,commands:string,path:string = 'httpdocs') {
+  yarnBuild(user:string,commands:string,path:string = 'httpdocs',timeout:number = 300 * 1000) {
     const login = `su - ${user} -s /bin/bash`
     const move = `cd ${path}`
     const install = `((${commands}) &> yarn-install.log)`
     const reboot = `mkdir -p tmp && touch tmp/restart.txt`
     const command = `${login} -c "${move} && ${install} && ${reboot}"`
-    return this.client.executeCommand(command,140 * 1000)
+    return this.client.executeCommand(command,timeout)
   }
 
-  prismaMigration(user:string,path:string = 'httpdocs') {
+  prismaMigration(user:string,path:string = 'httpdocs',timeout:number = 300 * 1000) {
     const login = `su - ${user} -s /bin/bash`
     const move = `cd ${path}`
     const migrate = `((yarn db:generate && yarn db:migrate:deploy) &> db-migration.log)`
     const reboot = `mkdir -p tmp && touch tmp/restart.txt`
     const command = `${login} -c "${move} && ${migrate} && ${reboot}"`
-    return this.client.executeCommand(command,140 * 1000)
+    return this.client.executeCommand(command,timeout)
   }
 
-  prismaSeeding(user:string,path:string = 'httpdocs') {
+  prismaSeeding(user:string,path:string = 'httpdocs',timeout:number = 300 * 1000) {
     const login = `su - ${user} -s /bin/bash`
     const move = `cd ${path}`
     const seed = `((yarn db:seed) &> db-seeding.log)`
     const command = `${login} -c "${move} && ${seed}"`
-    return this.client.executeCommand(command,140 * 1000)
+    return this.client.executeCommand(command,timeout)
   }
 
   disconnect(){
